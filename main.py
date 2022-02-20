c942e940f718653b2bd98d4fac208072584750d4
HackerZ-X <adnan1470369258@gmail.com>
2022-02-20 22:49:12 +0500

    tests

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-import glob
 import os
 
-# cwd = os.getcwd()
-# cwd = "D:"
-# cwdfiles = os.listdir()
-
-# logList = glob.glob(cwd + "/**/*.txt", recursive = True)
-# for file in logList:
-#     print(file)
-# def delete():
-#     logList = glob.glob(cwd + "/**/*.log", recursive = True)
-#     for file in logList:
-#         print(file)
-
-
-
-# def main():
-#     print(cwd)
-#     print(cwdfiles)
-#     print(delete())
-
-# main()
-# delete()
-# var = os.system('find . -type f -name "*.txt"')
 files = os.popen('find . -type f -name "*.log"').read().split('\n')[0:-1]
 files1 = os.popen('find . -type f -name "*.log.1"').read().split('\n')[0:-1]
 for count, file in enumerate(files):
     print(f"{count}: {file}")
+    if os.path.exists(file) and os.path.splitext(file)[-1] == '.log':
+        os.remove(file)
+        print(f"File removed: {file}")
+    else:
+        print(f"File not removed: {file}")
 for count, file in enumerate(files1):
     print(f"{count}: {file}")
+    print(f"{count}: {file}")
+    if os.path.exists(file) and os.path.splitext(file)[-1] == '.log.1':
+        os.remove(file)
+        print(f"File removed: {file}")
+    else:
+        print(f"File not removed: {file}")
